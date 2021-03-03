2db1b7883aa1a835aaa81913cda1386b1fc4c370
DanielTwin <danielgalicia@twinchat.com.mx>
2021-03-02 19:16:38 -0600

    modifications in categoria.php in ajax

--- ajax/categoria.php
+++ ajax/categoria.php
@@ -38,16 +38,20 @@ switch ($_GET["op"]){
 
         while ($reg=$rspta->fetch_object()){
             $data[]=[
-                "0" => $reg->idcategoria,
+                "0" => ($reg->condicion)?'<button class ="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+                    '<button class="btn btn-danger" onclick="desactivar('.$reg->idcategoria.')"><i class="fa fa-close"></i></button>':
+                    '<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+                    '<button class="btn btn-primary" onclick="activar('.$reg->idcategoria.')"><i class="fa fa-check"></i></button>',
                 "1" => $reg->nombre,
                 "2" => $reg->descripcion,
-                "3" => $reg->condicion,
+                "3" => ($reg->condicion)?'<span class="label bg-green">Activado</span>':
+                    '<span class="label bg-red">Desactivado</span>'
             ];
         }
         $results = [
-            "sEcho"=>1, //Información para el datatables
- 			"iTotalRecords"=>count($data), //enviamos el total registros al datatable
- 			"iTotalDisplayRecords"=>count($data), //enviamos el total registros a visualizar
+            "sEcho"=>1,                                 //Información para el datatables
+ 			"iTotalRecords"=>count($data),              //enviamos el total registros al datatable
+ 			"iTotalDisplayRecords"=>count($data),       //enviamos el total registros a visualizar
  			"aaData"=>$data
         ];
         echo json_encode($results);
