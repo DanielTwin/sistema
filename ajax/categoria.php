898742a7ba69d3059d2d9ed8c9e7459f67954d0c
DanielTwin <danielgalicia@twinchat.com.mx>
2021-03-03 19:13:56 -0600

    maintenance of relation tables and other funtionalities

--- ajax/categoria.php
+++ ajax/categoria.php
@@ -1,4 +1,17 @@
-<?
+<?php 
+ob_start();
+if (strlen(session_id()) < 1){
+	session_start();//Validamos si existe o no la sesión
+}
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: ../vistas/login.html");//Validamos el acceso solo a los usuarios logueados al sistema.
+}
+else
+{
+//Validamos el acceso solo al usuario logueado y autorizado.
+if ($_SESSION['almacen']==1)
+{
 require_once "../modelos/Categoria.php";
 
 $categoria=new Categoria();
@@ -8,53 +21,65 @@ $nombre=isset($_POST["nombre"])? limpiarCadena($_POST["nombre"]):"";
 $descripcion=isset($_POST["descripcion"])? limpiarCadena($_POST["descripcion"]):"";
 
 switch ($_GET["op"]){
-    case 'guardaryeditar':
-        if (empty($idcategoria)){
-            $rspta=$categoria->insertar($nombre,$descripcion);
-            echo $rspta ? "Categoria registrada" : "Categoria no se puede registrar";
-        }
-        else{
-            $rspta=$categoria->editar($idcategoria,$descripcion);
-            echo $rspta ? "Categoria actualizada" : "Categoria no se puede actualizar";
-        }
-        break;
-    case 'desactivar':
-        $rspta=$categoria->desactivar($idcategoria);
-        echo $rspta ? "Categoría desactivada" : "Categoría no se puede desactivar";
-        break;
-    case 'activar':
-        $rspta=$categoria->activar($idcategoria);
-        echo $rspta ? "Categoría activada" : "Categoría no se puede activar";
-        break;
-    case 'mostrar':
-        $rspta=$categoria->mostrar($idcategoria);
-        //Codificar el resultado utilizando json
-        echo json_encode($rspta);
-        break;
-    case 'listar':
-        $rspta=$categoria->listar();
-        //vamos a declara un array
-        $data = Array();
+	case 'guardaryeditar':
+		if (empty($idcategoria)){
+			$rspta=$categoria->insertar($nombre,$descripcion);
+			echo $rspta ? "Categoría registrada" : "Categoría no se pudo registrar";
+		}
+		else {
+			$rspta=$categoria->editar($idcategoria,$nombre,$descripcion);
+			echo $rspta ? "Categoría actualizada" : "Categoría no se pudo actualizar";
+		}
+	break;
 
-        while ($reg=$rspta->fetch_object()){
-            $data[]=[
-                "0" => ($reg->condicion)?'<button class ="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
-                    '<button class="btn btn-danger" onclick="desactivar('.$reg->idcategoria.')"><i class="fa fa-close"></i></button>':
-                    '<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
-                    '<button class="btn btn-primary" onclick="activar('.$reg->idcategoria.')"><i class="fa fa-check"></i></button>',
-                "1" => $reg->nombre,
-                "2" => $reg->descripcion,
-                "3" => ($reg->condicion)?'<span class="label bg-green">Activado</span>':
-                    '<span class="label bg-red">Desactivado</span>'
-            ];
-        }
-        $results = [
-            "sEcho"=>1,                                 //Información para el datatables
- 			"iTotalRecords"=>count($data),              //enviamos el total registros al datatable
- 			"iTotalDisplayRecords"=>count($data),       //enviamos el total registros a visualizar
- 			"aaData"=>$data
-        ];
-        echo json_encode($results);
-        break;
+	case 'desactivar':
+		$rspta=$categoria->desactivar($idcategoria);
+ 		echo $rspta ? "Categoría Desactivada" : "Categoría no se puede desactivar";
+	break;
+
+	case 'activar':
+		$rspta=$categoria->activar($idcategoria);
+ 		echo $rspta ? "Categoría activada" : "Categoría no se puede activar";
+	break;
+
+	case 'mostrar':
+		$rspta=$categoria->mostrar($idcategoria);
+ 		//Codificar el resultado utilizando json
+ 		echo json_encode($rspta);
+	break;
+
+	case 'listar':
+		$rspta=$categoria->listar();
+ 		//Vamos a declarar un array
+ 		$data= Array();
+
+ 		while ($reg=$rspta->fetch_object()){
+ 			$data[]=[
+ 				"0"=>($reg->condicion)?'<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+ 					' <button class="btn btn-danger" onclick="desactivar('.$reg->idcategoria.')"><i class="fa fa-close"></i></button>':
+ 					'<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+ 					' <button class="btn btn-primary" onclick="activar('.$reg->idcategoria.')"><i class="fa fa-check"></i></button>',
+ 				"1"=>$reg->nombre,
+ 				"2"=>$reg->descripcion,
+ 				"3"=>($reg->condicion)?'<span class="label bg-green">Activado</span>':
+ 				'<span class="label bg-red">Desactivado</span>'
+             ];
+ 		}
+ 		$results = [
+ 			"sEcho"=>1, //Información para el datatables
+ 			"iTotalRecords"=>count($data), //enviamos el total registros al datatable
+ 			"iTotalDisplayRecords"=>count($data), //enviamos el total registros a visualizar
+ 			"aaData"=>$data];
+ 		echo json_encode($results);
+
+	break;
 }
+//Fin de las validaciones de acceso
+}
+else
+{
+  require 'noacceso.php';
+}
+}
+ob_end_flush();
 ?>
